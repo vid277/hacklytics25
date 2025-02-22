37a6e73af90efe8e077f4d51b01e76c2f82c17ef
Anant Khandelwal <anantk2006@gmail.com>
2025-02-22 01:32:32 -0500

    working image creation

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI, File, UploadFile, Form
 import docker
 import shutil
 import os
+from fastapi.middleware.cors import CORSMiddleware
+
+from supabase import create_client, Client
+import uuid
+
+import boto3
+
+supabase_url = "https://pristirosscbgmkblozz.supabase.co"
+supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByaXN0aXJvc3NjYmdta2Jsb3p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyMDI2MzQsImV4cCI6MjA1NTc3ODYzNH0.7NWqkC5MUndwTxuLGlyBIEskItFzJ3M8iAKcARc_1yM"
+supabase = create_client(supabase_url, supabase_key)
+
+
+
 
 app = FastAPI()
 client = docker.from_env()
@@ -9,26 +22,54 @@ client = docker.from_env()
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-@app.post("/upload-docker/")
-async def upload_docker(user_id: str = Form(...), file: UploadFile = File(...)):
-    """
-    Accepts a Docker image tar file, saves it temporarily, and loads it into Docker.
-    """
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+def insert_job(user_id, job_id, compute_type, timeout, output_directory):
+    supabase.table("jobs").insert([{"user_id": user_id, "job_id": job_id, "compute_type": compute_type, "timeout": timeout, "output_directory": output_directory}]).execute()
+
+@app.post("/create-job/")
+async def create_job(user_id: str = Form(...), file: UploadFile = File(...), compute_type = 'cpu', timeout = 1, output_directory = 'output'):
+   
     file_path = os.path.join(UPLOAD_DIR, f"{user_id}.tar")
 
-    # Save the uploaded file
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    # Load image into Docker
+    job_id = str(uuid.uuid4())
+   
     try:
         with open(file_path, "rb") as image_file:
             image = client.images.load(image_file.read())[0]  # Load and get first image
-        image.tag(f"user_{user_id}:latest")  # Tag the image
+        tag = f"308832414989.dkr.ecr.us-east-1.amazonaws.com/hacklytics/storage:{job_id}:latest"
+        image.tag(tag)
+        
+        ecr_client = boto3.client('ecr')
+
+   
+        response = ecr_client.get_authorization_token()
+        username, password = (
+            boto3.utils.base64_decode(response['authorizationData'][0]['authorizationToken'])
+            .decode('utf-8')
+            .split(':')
+        )
+        registry = response['authorizationData'][0]['proxyEndpoint']
+
+        client.login(username=username, password=password, registry=registry)
+
+        push_logs = client.images.push(tag)
+        print("push logs:\n", push_logs)
+
+
     except Exception as e:
         return {"status": "error", "message": str(e)}
 
-    # Remove the file after loading
-    os.remove(file_path)
+    
+    
 
-    return {"status": "success", "message": f"Docker image for user {user_id} uploaded and tagged."}
+    return {"status": "success", "message": f"Job id {job_id} created successfully"}
